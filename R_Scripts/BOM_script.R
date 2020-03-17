02d7974e1c30e9d806065fb3b83c2c4cc72dcd63
Bovill <bov004@csiro.au>
2020-03-17 16:17:12 +1100

    finished for the day

--- R_Scripts/BOM_script.R
+++ R_Scripts/BOM_script.R
@@ -18,29 +18,74 @@ colnames(BOM_Stations)
 
 separate(BOM_Data, Temp_min_max, into = c("T_Min", "T_Max"), sep = "/") -> BOM_Data 
 
-BOM_Data %>% 
+#converting various columns to numeric to force NA's
+BOM_Data %>%
   mutate(T_Min = as.numeric(T_Min)) %>% 
   mutate(T_Max = as.numeric(T_Max)) %>% 
   mutate(Rainfall = as.numeric(Rainfall))-> BOM_Data
 
-summary(BOM_Data)
-
+#This gives which stations which have data for each column individually
 BOM_Data %>% 
   group_by(Station_number) %>%
   summarise(MinDays = sum(!is.na(T_Min)),
             MaxDays = sum(!is.na(T_Max)),
             RainyDays = sum(!is.na(Rainfall)))
-  
+
+#This gives which stations have all three measurements
+#Uncomment out the summarise function will create a table with all three
+#But comment out the last summarise if above line is done!
+BOM_Data %>% 
+  group_by(Station_number) %>%
+  filter(!is.na(T_Min)) %>% 
+  filter(!is.na(T_Max)) %>% 
+  filter(!is.na(Rainfall)) %>% 
+  #summarise(MinDays = sum(!is.na(T_Min)),
+  #          MaxDays = sum(!is.na(T_Max)),
+  #          RainyDays = sum(!is.na(Rainfall)))
+  summarise(n = n())
+        
+
 #Question 2
 #Which month saw the lowest average daily temperature difference?
 
-BOM_Data %>%
-  mutate(Temp_Diff = T_Max - T_Min) -> BOM_Data %>% 
-  group_by(Month) %>% 
-  
+BOM_Data %>% 
+  mutate(Temp_Diff = T_Max - T_Min) %>% 
+  group_by(Month) %>%
+  summarise(Ave_Month_Temp_Diff = mean(Temp_Diff, na.rm = TRUE)) %>% 
+  arrange(Ave_Month_Temp_Diff)
 
 #Question 3
 #Which state saw the lowest average daily temperature difference?
+
+#gather and spread to get this into a decent format to work with
+BOM_Stations %>% 
+  gather(key = key, value = value, 2:21) %>% 
+  rename(Station_number = key) %>% 
+  spread(BOM_Stations, key = info, value = value) -> BOM_Stations
+
+glimpse(BOM_Stations)
+glimpse(BOM_Data)
+
+#converting to same format between both objects
+BOM_Stations %>%
+  mutate(Station_number = as.numeric(Station_number)) %>% 
+  mutate(elev = as.numeric(elev)) %>%
+  mutate(end = as.numeric(end)) %>%
+  mutate(lat = as.numeric(lat)) %>% 
+  mutate(lon = as.numeric(lon)) %>% 
+  mutate(name = as.character(name)) %>% 
+  mutate(start = as.numeric(start)) %>% 
+  mutate(state = as.character(state)) -> BOM_Stations
+
+#joining both objects
+full_join(BOM_Data, BOM_Stations) -> BOM_Combined
+
+#finally answering the question!
+BOM_Combined %>%
+  mutate(Temp_Diff = T_Max - T_Min) %>% 
+  group_by(state) %>%
+  summarise(Ave_Temp_Diff = mean(Temp_Diff, na.rm = TRUE)) %>% 
+  arrange(Ave_Temp_Diff)
 
 #Question 4
 #Does the westmost (lowest longitude) or eastmost (highest longitude)
